4c1febfb482e631f9fc7fe4344d8e1341fa7b56d
Chris Hedgate <chris@hedgate.net>
2014-12-22 21:34:07 +0100

    completed video listing and showing a video

--- README.md
+++ README.md
@@ -1,32 +1,7 @@
-# button-sample
+# Example for uploading and showing videos using Panda
 
-A barebones Node.js app using [Express 4](http://expressjs.com/).
+This very basic Node.js app uses Panda (http://www.pandastream.com) to upload and encode videos. The video files are stored on Amazon S3 (though Panada has support for Google Cloud, Rackspace Cloud Files and other options as well).
 
-## Running Locally
+To be able to run the code requires an account at Panda to be setup, with the access key, secret key and cloud id present as environment variables. Also, for Panda to work an Amazon S3 account needs to exist (with permissions for Panda to manage files) and the bucket name and S3 url also needs to exist as environment variables. See the example.env file to use with Foreman for which environment variables are required.
 
-Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
-
-```sh
-git clone git@github.com:heroku/button-sample.git # or clone your own fork
-cd button-sample
-npm install
-npm start
-```
-
-Your app should now be running on [localhost:5000](http://localhost:5000/).
-
-## Deploying to Heroku
-
-```
-heroku create
-git push heroku master
-heroku open
-```
-
-Alternatively, you can deploy your own copy of the app using this button:
-
-[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
-
-## Documentation
-
-For more information about using Heroku, check out https://devcenter.heroku.com/
+To run the app locally, make sure everything is installed (Node, npm, npm install all modules) and run using Foreman (foreman start -e your-env-file-name.env). The app can also be uploaded to Heroku without any modifications (after creating the necessary environment variables of course).
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var express = require('express')
+var bodyParser = require('body-parser');
+var panda = require('panda');
+
+var config = {
+  accessKey: process.env.ACCESS_KEY,
+  secretKey: process.env.SECRET_KEY,
+  cloudId: process.env.CLOUD_ID
+}
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -6,8 +15,55 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 
+app.use(bodyParser());
+
 app.get('/', function(request, response) {
   response.render('index.html')
+})
+
+app.post('/panda/authorize_upload', function(request, response) {
+  var client = panda.createClient(config);
+  var payload = JSON.parse(request.body.payload);
+  var url;
+  var options = {};
+  if (payload.filename) {
+    url = '/videos/upload.json';
+    options.file_name = payload.filename;
+    options.file_size = payload.filesize;
+    options.profiles = "h264";
+  } else {
+    url = '/videos.json';
+    options.source_url = payload.fileurl;
+  }
+  client.post(url, options, function(result) {
+    console.log(result);
+    response.json({upload_url: result.location});
+  });
+})
+
+app.get('/upload', function(request, response) {
+  response.render('upload.html')
+})
+
+app.post('/upload', function(request, response) {
+  // response.json({video_file_id: request.params.panda_video_id})
+  response.redirect('/videos/'+request.params.panda_video_id);
+})
+
+app.get('/videos', function(request, response) {
+  var client = panda.createClient(config);
+  client.get('/videos.json', {}, function(videos) {
+    response.render('videos.html', {videos: videos});
+  });
+})
+
+app.get('/videos/:id', function(request, response) {
+  var client = panda.createClient(config);
+  client.get('/videos/'+request.params.id+'.json', {}, function(video) {
+    client.get('/videos/'+request.params.id+'/encodings.json', {}, function(encodings) {
+      response.render('video.html', {video: video, encoding: encodings[0]});
+    });
+  });
 })
 
 app.listen(app.get('port'), function() {
